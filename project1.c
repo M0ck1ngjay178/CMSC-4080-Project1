8d2cb062a026459d52063ef319e70138b658bb0c
M0ck1ngjay178 <bon8330@pennwest.edu>
2025-02-08 16:34:12 -0500

    file set up

---
+++ project1.c
@@ -0,0 +1,25 @@
+//---------LIBRARIES---------
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+//-------------------------
+
+//-----Prototypes-----
+void subcriptMethod();
+//-------------------
+
+
+//===========MAIN=================
+int main(void){
+
+
+
+    return 0;
+}
+//===========END MAIN=================
+
+
+void subcriptMethod(){
+    
+}
+
