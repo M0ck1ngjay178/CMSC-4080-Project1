97e5c1562b3d7fee98efd89203164fc5ced88a9d
M0ck1ngjay178 <bon8330@pennwest.edu>
2025-02-08 17:34:43 -0500

    revised pointer file with malloc

--- Pointers/project1_Pointer.c
+++ Pointers/project1_Pointer.c
@@ -5,14 +5,74 @@
 //-------------------------
 
 //-------DEFINED VARIABLES-----
-#define SIZE 500
+#define SIZE 1000
 //----------------------------
 
 //-----Prototypes-----
 void pointerMethod();
+void freeArray(int **arr, int size);
+int **allocateArr(int size);
 //-------------------
 
 
+//===========MAIN=================
+int main(void){
+
+    pointerMethod();
+
+    return 0;
+}
+//===========END MAIN=================
+
+//---------------FUNCTON BODIES--------------------------
+void pointerMethod(){
+    //int subArr[SIZE][SIZE];
+    int **subArr = allocateArr(SIZE);
+    int i, j;
+    clock_t begin, finish;
+    double totalTime;
+
+    begin = clock();
+
+    for(i = 0; i < SIZE; i++){
+        for(j=0; j < SIZE; j++){
+            subArr[i][j] = i+j;
+            //printf("This is the Contents: ", subArr);
+        }
+    }
+    finish = clock();
+    totalTime =((double)(finish-begin)/CLOCKS_PER_SEC);
+    printf("Subscript Method: %f sec\n", totalTime);
+    freeArray(subArr, SIZE);
+}
+//----------ALLOCATE-------------
+int **allocateArr(int size) {
+    int **arr = (int **)malloc(size * sizeof(int *));
+    if (!arr) {
+        perror("Allocation Error");
+        exit(EXIT_FAILURE);
+    }
+    
+    for (int i = 0; i < size; i++) {
+        arr[i] = (int *)malloc(size * sizeof(int));
+        if (!arr[i]) {
+            perror("Allocation Error");
+            exit(EXIT_FAILURE);
+        }
+    }
+    return arr;
+}
+
+//-----------free memory------------
+void freeArray(int **arr, int size) {
+    for (int i = 0; i < size; i++) {
+        free(arr[i]);
+    }
+    free(arr);
+}
+
+
+/*
 //===========MAIN=================
 int main(void){
 
@@ -42,3 +102,4 @@ void pointerMethod(){
     printf("Pointer Method: %f sec\n", totalTime);
 }
 //-------------------------------------------------------------------
+*/
