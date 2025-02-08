c033036f54157fc7cf1a5426c7b93e2510f28fcc
M0ck1ngjay178 <bon8330@pennwest.edu>
2025-02-08 16:52:38 -0500

    added subscriptMethod/main, comments

--- project1.c
+++ project1.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 //-------------------------
 
+#define SIZE 900
 //-----Prototypes-----
 void subcriptMethod();
 //-------------------
@@ -12,7 +13,7 @@ void subcriptMethod();
 //===========MAIN=================
 int main(void){
 
-
+    subcriptMethod();
 
     return 0;
 }
@@ -20,6 +21,20 @@ int main(void){
 
 
 void subcriptMethod(){
-    
+    int subArr[SIZE][SIZE];
+    int i, j;
+    clock_t begin, finish;
+    double totalTime;
+
+    begin = clock();
+
+    for(i = 0; i < SIZE; i++){
+        for(j=0; j < SIZE; j++){
+            subArr[i][j] = i+j;
+        }
+    }
+    finish = clock();
+    totalTime =((double)(begin-finish)/CLOCKS_PER_SEC);
+    printf("Subscript Method: %f sec\n", totalTime);
 }
 
