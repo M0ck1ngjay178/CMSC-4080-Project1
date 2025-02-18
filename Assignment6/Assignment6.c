e1d567708f8f7b6dbcd1d152c8d01cb34618ec35
M0ck1ngjay178 <bon8330@pennwest.edu>
2025-02-18 08:52:48 -0500

    commented Finalv

--- Assignment6/Assignment6.c
+++ Assignment6/Assignment6.c
@@ -7,33 +7,31 @@
 #include <stdio.h>
 //--------------------------
 
-int fun(int *a);
+int fun(int *a);//fun prototype of type int
 
-//int a, b;
 
 int main (void){
 
-    int a, b;
-    a = 10;
+    int a, b; //initialize integer variables for a and b
+    a = 10;  //set a to 10
 
-    b = a + fun(&a);
+    b = a + fun(&a); //call fun() from the right
     printf("\n\n---------------RIGHT----------------\n");
     printf("With the function call on the right, ");
-    printf(" b is: %d\n", b);
+    printf(" b is: %d\n", b);                               //display the calculated result stored in b
     printf("\n-------------------------------------\n\n");
 
-    a = 10;
-    b = fun(&a) + a;
+    a = 10;  //set a back to 10
+    b = fun(&a) + a;    //call fun() from the left
     printf("\n\n----------------LEFT------------------\n");
     printf("With the function call on the left, ");
-    printf(" b is: %d\n", b);
+    printf(" b is: %d\n", b);                               //display the calculated result stored in b
     printf("\n-------------------------------------\n\n");
 
+    return 0;   //return 0 upon exit
+}//end main
 
-    return 0;
-}
-
-int fun(int *a){
-    *a +=10;
-    return 40;
+int fun(int *a){    //fun function, implemets 2way parameters or pass-by-reference
+    *a +=10;        //pass value of a, perform calculation
+    return 3;       //retun any value 
 }
