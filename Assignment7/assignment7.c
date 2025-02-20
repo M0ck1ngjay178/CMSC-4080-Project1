c0b9162d694142fae75b234070fcc1503b257835
M0ck1ngjay178 <bon8330@pennwest.edu>
2025-02-19 20:18:03 -0500

    built shell structure of cases

--- Assignment7/assignment7.c
+++ Assignment7/assignment7.c
@@ -14,40 +14,49 @@ void welcome(void);
 void mathCheck(void);
 //-----------------End Prototypes-----------------
 
-void main(void){
+int main(void){
 
     int choose;
     bool look = true;
     welcome();
-    while(look){
     
+    while(look){
         menu();
-
-        scanf("Enter Choice: %d", &choose);
+        //printf("Enter Choice: ");
+        scanf("%d", &choose);
 
         switch(choose){
 
             case 1:
+                
                 break;
             case 2:
                 break;
             case 3:
+                mathCheck();
                 break;
+            case 4:
+                printf("Exiting ...");
+                return 0;
             default:
                 printf("!!!Invalid choice!!! Enter New Choice: ");
+                //break;
         }
 
     }
 
-    
-    
     return 0;
 }
 
 //-----------------Function Bodies--------------------
 void menu(void){
     printf("\n--------MENU--------\n");
-    printf("--------------------\n");
+    printf("1. ");
+    printf("2  ");
+    printf("3. Test Division By Zero\n");
+    printf("4. Exit\n");
+    printf(" -Choose Option: \n");
+    printf("----------------------\n");
 }
 
 void welcome(void){
@@ -57,9 +66,13 @@ void welcome(void){
 }
 void mathCheck(void){
    int a, b;
-   if( a && (b/13-1)){
-    
-   }
-
+   //if( a && (b/13-1)){
+    //.....
+    if( a && (10/ b)){
+        printf("Both Variables were evaluated as True!");
+    }else{
+        printf("Short-Circuit! Didnt divide by zero!");
+    }
 }
+
 //-----------------End Function Bodies----------------
